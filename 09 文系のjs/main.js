b96d8a0523d6fa345efa5c78c6ee13216e8db435
nnm <m.n.for.programming>
2022-02-07 12:52:34 +0900

    あ

--- 09 文系のjs/main.js
+++ 09 文系のjs/main.js
@@ -7,6 +7,11 @@ console.log('Hello World!!');
   const p = document.getElementById('p_id');
   console.log(`p.id:${p.id}`);
   
+  const a = document.getElementById('link');
+  const img = a.firstElementChild;
+  console.log(`元のリンクURL: ${a.href}`);
+  a.href = 'https://loremflickr.com/320/240/dog';
+  
   let text;
   if (a.target  === '_blank') {
     text = '別のタブで開きます';
@@ -19,13 +24,6 @@ console.log('Hello World!!');
   const p = document.getElementsByTagName('p')[1];
   p.appendChild(br);
   p.appendChild(textNode);
-  
-  const a = document.getElementById('link');
-  const img = a.firstElementChild;
-  console.log(`元のリンクURL: ${a.href}`);
-  a.href = 'https://loremflickr.com/320/240/dog';
-  console.log(`元の画像ソース: ${img.src}`);
-  img.src = 'https://loremflickr.com/320/240/dog';
   
   const p = document.querySelector('p:nth-of-type(2)');
   console.log(p.className);
